9a2abfa1fb14cbea56bcd3d1f208715231899137
Chuks-project <chuksy237@yahoo.com>
2023-03-25 16:20:29 +0000

    added more files

--- Modules/Autoscaling/wordpress.sh
+++ Modules/Autoscaling/wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /var/www/
-sudo mount -t efs -o tls,accesspoint=fsap-0f9364679383ffbc0 fs-8b501d3f:/ /var/www/
+sudo mount -t efs -o tls,accesspoint=fsap-0a298859d5d5fc9ac fs-017fdccebb160eb94:/ /var/www/
 yum install -y httpd 
 systemctl start httpd
 systemctl enable httpd
@@ -17,12 +17,20 @@ mkdir /var/www/html/
 cp -R /wordpress/* /var/www/html/
 cd /var/www/html/
 touch healthstatus
-sed -i "s/localhost/acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com/g" wp-config.php 
-sed -i "s/username_here/ACSadmin/g" wp-config.php 
-sed -i "s/password_here/admin12345/g" wp-config.php 
+sed -i "s/localhost/vic-database.cgtcuv9uf0id.eu-west-1.rds.amazonaws.com/g" wp-config.php 
+sed -i "s/username_here/vicadmin/g" wp-config.php 
+sed -i "s/password_here/vic12345/g" wp-config.php 
 sed -i "s/database_name_here/wordpressdb/g" wp-config.php 
 chcon -t httpd_sys_rw_content_t /var/www/html/ -R
 systemctl restart httpd
+
+
+
+
+
+
+
+
 
 
 
---
+++ Modules/Security/outputs.tf
@@ -0,0 +1,28 @@
+output "ALB-sg" {
+  value = aws_security_group.vic["ext-alb-sg"].id
+}
+
+
+output "IALB-sg" {
+  value = aws_security_group.vic["int-alb-sg"].id
+}
+
+
+output "bastion-sg" {
+  value = aws_security_group.vic["bastion-sg"].id
+}
+
+
+output "nginx-sg" {
+  value = aws_security_group.vic["nginx-sg"].id
+}
+
+
+output "web-sg" {
+  value = aws_security_group.vic["webserver-sg"].id
+}
+
+
+output "datalayer-sg" {
+  value = aws_security_group.vic["datalayer-sg"].id
+}
